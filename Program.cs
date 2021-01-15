7d506eb58989e744b86fca34e694cdbaf3a35d8e
Poisoned_Vine <klarell@gmail.com>
2021-01-15 22:22:36 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Say your name");
+            string Name = Console.ReadLine();
+            Console.WriteLine($"Hello, {Name}. Today is {DateTime.UtcNow}");
+        }
+    }
+}
