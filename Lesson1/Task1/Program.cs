ab8d4955587bc0bff75aa7c1be88cf97c115a7d1
Poisoned_Vine <klarell@gmail.com>
2021-01-16 21:25:46 +0300

    Try to create task folders

---
+++ Lesson1/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("Введите имя");
+            string Name = Console.ReadLine();
+            Console.WriteLine($"Привет, {Name}, сегодня {DateTime.Today.ToString("dd.MM.yyyy")}");
+        }
+    }
+}
