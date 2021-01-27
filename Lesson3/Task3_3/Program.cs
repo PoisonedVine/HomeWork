9ddfb9d0c8d80b25cdf2c356e67164b1a159f071
Poisoned_Vine <klarell@gmail.com>
2021-01-27 12:07:34 +0300

    Урок 3. Задача 3

---
+++ Lesson3/Task3_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string CurString;
+            int j = 0;
+                        
+            Console.WriteLine("Введите произвольную строку");
+            CurString = Console.ReadLine();
+            char[] NewString = new char[CurString.Length];
+
+            Console.WriteLine();
+            for (int i = CurString.Length-1; i >= 0; i--) {
+                NewString[j] = CurString[i];
+                j++;
+            }
+            Console.WriteLine(NewString);
+        }
+    }
+}
