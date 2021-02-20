bf81f187ee221393c823020a87ea64e4d3ea97d5
Poisoned_Vine <klarell@gmail.com>
2021-02-20 16:56:48 +0300

    Урок 1. Задача 2

---
+++ Part2/Lesson1/Task2_1_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task2_1_2
+{
+    class Program
+    {
+        public static int StrangeSum(int[] inputArray) // O(N^3)
+        {
+            int sum = 0; //const
+            for (int i = 0; i < inputArray.Length; i++) // O(N)
+            {
+                for (int j = 0; j < inputArray.Length; j++) //O(N)
+                {
+                    for (int k = 0; k < inputArray.Length; k++) // O(N)
+                    {
+                        int y = 0; //const
+
+                        if (j != 0) //const
+                        {
+                            y = k / j; //const
+                        }
+
+                        sum += inputArray[i] + i + k + j + y; //const
+                    }
+                }
+            }
+
+            return sum; //const
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
