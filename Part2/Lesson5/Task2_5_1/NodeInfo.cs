89b7a830e8b0b8f82fa0de594ac498cfe56d7e31
Poisoned_Vine <klarell@gmail.com>
2021-03-11 11:28:49 +0300

    Урок 5. Задача 1

---
+++ Part2/Lesson5/Task2_5_1/NodeInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace Task2_5_1
+{
+    public class NodeInfo
+    {
+        public int Depth { get; set; }
+        public TreeNode Node { get; set; }
+
+        public string Position { get; set; }
+    }
+}
