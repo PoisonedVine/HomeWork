cf8146ab4918105e4329bcef63be24853a383d9a
Poisoned_Vine <klarell@gmail.com>
2021-05-05 19:23:25 +0300

    temp 3

---
+++ MetricsManager/Controllers/CrudController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsManager.Controllers
+{
+    [Route("api/crud")]
+    [ApiController]
+    public class CrudController : ControllerBase
+    {
+    }
+}
